53206ceebb0c252b8374fb63e8f59f190e1c9e22
clement-caillat <clement.caillat@laplateforme.io>
2022-05-14 12:55:05 +0200

    Finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Guessing Game made with Rust !");
+
+    let secret_number = rand::thread_rng().gen_range(1..101); 
+
+    loop {
+        println!("Please enter a number : ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small !"),
+            Ordering::Greater => println!("Too big !"),
+            Ordering::Equal => {
+                println!("You win !");
+                break;
+            }
+        }
+    }
+
 }
